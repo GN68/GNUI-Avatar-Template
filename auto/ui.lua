f9f1f6d33a9d4dafa9f81c419ff62a3d07f54938
GN68 <81744903+GN68@users.noreply.github.com>
2026-05-29 22:36:40 +0800

    default template

---
+++ auto/ui.lua
@@ -0,0 +1,86 @@
+
+if not host:isHost() then return end
+local GNUI = require("lib.GNUI.init")
+local screen = GNUI.getScreen()
+
+--────  UI Code  ────────────────────────────────────────────────────────--
+
+
+if true then
+	--- parent box to hold all the columns
+	local classColumns = screen:parse({
+		style = "opaque",
+		layout = "HORIZONTAL",
+		gap = 5,
+		padding = vec(3,3,3,3),
+	})
+
+	-- loop for each class with a given style
+	for _, className in ipairs(GNUI.Theme.getClassNames()) do
+		-- create a column container for each widget
+		local variantColumn = screen:parse({
+			layout = "VERTICAL",
+			sizing = { "FIT", "FIT" },
+			minSize = vec(80, 0),
+			gap = 5,
+		})
+		classColumns:addChild(variantColumn)
+
+		-- create the class header
+		local classHeader = screen:parse({
+			sizing = { "FILL", "FIT" },
+			minSize = vec(0, 15),
+			text = className,
+			style = "opaque",
+		})
+		variantColumn:addChild(classHeader)
+
+		-- loop for each class variant
+		for _, variantName in ipairs(GNUI.Theme.getVariantNames(className)) do
+			-- create that given widget with the given variant
+			local widget = screen:parse({
+				type = className,
+				sizing = { "FILL", "FIT" },
+				style = variantName,
+				text = variantName,
+			})
+			variantColumn:addChild(widget)
+
+			--if className == "button" then
+			--	widget.PRESSED:register(function ()
+			--		widget:free()
+			--	end)
+			--end
+		end
+	end
+	classColumns:setPos(5, 5)
+	screen:addChild(classColumns)
+end
+
+
+--────  GNUI Boilerplate  ────────────────────────────────────────────────────────--
+-- TODO: make all this boilerplate code a loadable preset instead
+events.KEY_PRESS:register(function(key, state)
+	local cancel = screen:inputKey(key, state)
+	if cancel then
+		host:setChatText("")
+	end
+	return cancel
+end)
+
+events.CHAR_TYPED:register(function(char, modifiers, codepoint) screen:inputChar(char) end)
+events.MOUSE_PRESS:register(function(button, state) screen:inputMouse(button, state) end)
+events.MOUSE_SCROLL:register(function(amount) screen:inputScroll(amount, 0) end)
+
+function events.WORLD_RENDER()
+	local screenID = host:getScreen()
+	if (action_wheel:isEnabled() or screenID) and not screenID == "net.minecraft.class_408" then -- move mouse away if theres already UI open
+		screen:setCursorPos(-1000, -1000)
+	else
+		screen:setCursorPos(client:getMousePos() *
+			(client:getScaledWindowSize() / client:getWindowSize()))
+	end
+	screen:flushUpdates()
+end
+
+screen.display:setParentType("HUD")
